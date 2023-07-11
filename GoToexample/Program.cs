22d038bdf945e6437f2958922ffab46b018cde12
sindhusunki789@gmail.com <sindhusunki789@gmail.com>
2023-07-11 11:53:49 +0530

    first commit

---
+++ GoToexample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoToexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName;
+            Console.WriteLine("Enter User Name");
+            UserName = Console.ReadLine();
+            if(UserName.Length>=6)
+            {
+                Console.WriteLine("Welcome : \t" + UserName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid User Name");
+            }
+            Console.ReadKey();
+        }
+    }
+}
